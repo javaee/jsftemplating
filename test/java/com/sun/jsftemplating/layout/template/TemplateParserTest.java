0c3a92933b9d8ee34930ba57743a4aea8ff38c4f
Ken Paulsen <kenpaulsen@java.net>
2006-04-20 10:01:25 +0000

    Add a couple test files.
    svn path=/trunk/; revision=4

---
+++ test/java/com/sun/jsftemplating/layout/template/TemplateParserTest.java
@@ -0,0 +1,254 @@
+package com.sun.jsftemplating.layout.template;
+
+import java.net.URL;
+
+import junit.framework.*;
+
+
+/**
+ *  <p>	Tests for the "Template" parser.</p>
+ */
+public class TemplateParserTest extends TestCase {
+    protected void setUp() {
+    }
+
+    public void testURL() {
+	try {
+	    TemplateParser parser = new TemplateParser(
+		new URL("file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt"));
+	    assertEquals("URL", "file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt", parser.getURL().toString());
+	} catch (Exception ex) {
+	    fail(ex.getMessage());
+	}
+    }
+
+    public void testOpenClose() {
+	try {
+	    TemplateParser parser = new TemplateParser(
+		new URL("file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt"));
+	    parser.open();
+	    parser.close();
+	} catch (Exception ex) {
+	    fail(ex.getMessage());
+	}
+    }
+
+/*
+    public void testInclude() {
+    }
+*/
+
+    public void testNextChar1() {
+	try {
+	    TemplateParser parser = new TemplateParser(
+		new URL("file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt"));
+	    parser.open();
+	    assertEquals("testNextChar1-1", '#', parser.nextChar());
+	    assertEquals("testNextChar1-2", ' ', parser.nextChar());
+	    assertEquals("testNextChar1-3", 'R', parser.nextChar());
+	    assertEquals("testNextChar1-4", 'e', parser.nextChar());
+	    assertEquals("testNextChar1-5", 'a', parser.nextChar());
+	    assertEquals("testNextChar1-6", 'd', parser.nextChar());
+	    assertEquals("testNextChar1-7", 'e', parser.nextChar());
+	    assertEquals("testNextChar1-8", 'r', parser.nextChar());
+	    assertEquals("testNextChar1-9", ' ', parser.nextChar());
+	    parser.close();
+	} catch (Exception ex) {
+	    fail(ex.getMessage());
+	}
+    }
+
+    public void testUnread() {
+	try {
+	    TemplateParser parser = new TemplateParser(
+		new URL("file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt"));
+	    parser.open();
+	    assertEquals("testNextChar1-1", '#', parser.nextChar());
+	    assertEquals("testNextChar1-2", ' ', parser.nextChar());
+	    parser.unread(' ');
+	    assertEquals("testNextChar1-3", ' ', parser.nextChar());
+	    assertEquals("testNextChar1-4", 'R', parser.nextChar());
+	    parser.unread('R');
+	    assertEquals("testNextChar1-5", 'R', parser.nextChar());
+	    parser.unread('X');
+	    assertEquals("testNextChar1-6", 'X', parser.nextChar());
+	    assertEquals("testNextChar1-7", 'e', parser.nextChar());
+	    parser.close();
+	} catch (Exception ex) {
+	    fail(ex.getMessage());
+	}
+    }
+
+    public void testNVP() {
+	try {
+	    TemplateParser parser = new TemplateParser(
+		new URL("file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt"));
+	    parser.open();
+	    // Read some lines
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+
+	    // Move in to the NVP (actually 1 past just to see that that works)
+	    parser.nextChar(); parser.nextChar(); parser.nextChar(); parser.nextChar(); parser.nextChar();
+	    parser.nextChar(); parser.nextChar(); parser.nextChar(); parser.nextChar(); parser.nextChar();
+	    parser.nextChar(); parser.nextChar(); parser.nextChar(); parser.nextChar();
+
+	    TemplateParser.NameValuePair nvp = parser.getNVP();
+
+	    assertEquals("testNVP1", "ile", nvp.getName());
+	    assertEquals("testNVP2", "jsftemplating/js/jsftemplating.js", nvp.getValue());
+	    assertEquals("testNVP3", null, nvp.getTarget());
+
+	    parser.close();
+	} catch (Exception ex) {
+	    fail(ex.getMessage());
+	}
+    }
+
+    public void testNVP2() {
+	try {
+	    TemplateParser parser = new TemplateParser(
+		new URL("file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt"));
+	    parser.open();
+
+	    // Read 49 lines
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+
+	    // Move to the output mapping on line 50
+	    parser.readUntil('v');
+	    parser.unread('v');
+
+	    TemplateParser.NameValuePair nvp = parser.getNVP();
+
+	    assertEquals("testNVP1", "value", nvp.getName());
+	    assertEquals("testNVP2", "val", nvp.getValue());
+	    assertEquals("testNVP3", "attribute", nvp.getTarget());
+
+	    parser.close();
+	} catch (Exception ex) {
+	    fail(ex.getMessage());
+	}
+    }
+
+    public void testReadLine() {
+	try {
+	    TemplateParser parser = new TemplateParser(
+		new URL("file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt"));
+	    parser.open();
+	    // Read some lines
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+
+	    // Read some characters
+	    parser.nextChar(); parser.nextChar(); parser.nextChar(); parser.nextChar(); parser.nextChar();
+	    parser.nextChar(); parser.nextChar(); parser.nextChar(); parser.nextChar(); parser.nextChar();
+
+	    // Make sure we're at the right spot
+	    assertEquals("testReadLine1", ' ', parser.nextChar());
+	    assertEquals("testReadLine2", 'm', parser.nextChar());
+
+	    // Make sure we can read the rest of the line
+	    assertEquals("testReadLine3", "ulti line comment is hit, skip until end is found", parser.readLine());
+
+	    // Read more lines
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+
+	    // Make sure we're at the right spot
+	    assertEquals("testReadLine4", "\t    <script file=\"jsftemplating/js/jsftemplating.js\" />", parser.readLine());
+	    parser.close();
+	} catch (Exception ex) {
+	    fail(ex.getMessage());
+	}
+    }
+
+    public void testReadToken() {
+	try {
+	    TemplateParser parser = new TemplateParser(
+		new URL("file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt"));
+	    parser.open();
+	    // Read some lines
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+
+	    // Move to the Script Tag
+	    parser.readUntil('<');
+
+	    // Test readToken()
+	    assertEquals("testReadToken", "script", parser.readToken());
+
+	    // Test skipWhiteSpace();
+	    parser.skipWhiteSpace(TemplateParser.SIMPLE_WHITE_SPACE);
+
+	    // Test NVP
+	    TemplateParser.NameValuePair nvp = parser.getNVP();
+
+	    // NVP should be setup correctly
+	    assertEquals("testNVP1", "file", nvp.getName());
+	    assertEquals("testNVP2", "jsftemplating/js/jsftemplating.js", nvp.getValue());
+	    assertEquals("testNVP3", null, nvp.getTarget());
+
+	    parser.close();
+	} catch (Exception ex) {
+	    fail(ex.getMessage());
+	}
+    }
+
+    /**
+     *	This test tests the String version of Read Until.
+     */
+    public void testReadUntilStr() {
+	try {
+	    TemplateParser parser = new TemplateParser(
+		new URL("file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt"));
+	    parser.open();
+	    // Read some lines
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+	    parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine(); parser.readLine();
+
+	    // Test readUntil.  On line before, read until openning comment.
+	    assertEquals("testReadUntilStr", "    // This text should be commented out.  <tags> should not be parsed.\n    <!--", parser.readUntil("<!--"));
+	    assertEquals("testReadUntilStr2", "\n	This text should be commented out.  <tags> should not be parsed.\n    -->", parser.readUntil("-->"));
+	    assertEquals("testReadUntilStr3", "\n    /*\n     *	This text should be commented out.  <tags> should not be parsed.\n     */", parser.readUntil("*/"));
+
+	    parser.close();
+	} catch (Exception ex) {
+	    fail(ex.getMessage());
+	}
+    }
+
+/*
+    public void testAdd() {
+	assertTrue(5 == 6);
+    }
+
+    public void testEquals() {
+	assertEquals(12, 12);
+	assertEquals(12L, 12L);
+	assertEquals(new Long(12), new Long(12));
+
+	assertEquals("Size", 12, 13);
+	assertEquals("Capacity", 12.0, 11.99, 0.0);
+    }
+
+    public static void main (String[] args) {
+	    junit.textui.TestRunner.run(suite());
+    }
+*/
+}
---
+++ test/java/com/sun/jsftemplating/layout/template/TemplateReaderTest.java
@@ -0,0 +1,47 @@
+package com.sun.jsftemplating.layout.template;
+
+import com.sun.jsftemplating.layout.descriptors.LayoutDefinition;
+
+import java.net.URL;
+
+import junit.framework.*;
+
+
+/**
+ *  <p>	Tests for the {@link TemplateReader}.</p>
+ */
+public class TemplateReaderTest extends TestCase {
+
+    /**
+     *
+     */
+    protected void setUp() {
+    }
+
+    /**
+     *	<p> Simple test to ensure we can read a simple template.</p>
+     */
+    public void testRead1() {
+	try {
+	    TemplateReader reader =
+		new TemplateReader(new URL("file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt"));
+	    reader.read();
+	} catch (Exception ex) {
+	    fail(ex.getMessage());
+	}
+    }
+
+    /**
+     *	<p> This tests the accuracy of what was read.</p>
+     */
+    public void testRead2() {
+	try {
+	    TemplateReader reader =
+		new TemplateReader(new URL("file:src/java/com/sun/jsftemplating/layout/template/TemplateFormat.txt"));
+	    LayoutDefinition ld = reader.read();
+	    assertEquals("LayoutDefinition.unevaluatedId", "", ld.getUnevaluatedId());
+	} catch (Exception ex) {
+	    fail(ex.getMessage());
+	}
+    }
+}
