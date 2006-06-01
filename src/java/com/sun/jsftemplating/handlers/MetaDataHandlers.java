f88c2d4cda58d2df4c971d079b9530a86eb86c3e
Ken Paulsen <kenpaulsen@java.net>
2006-06-01 01:14:17 +0000

    * Added method to get global ComponentTypes
    svn path=/trunk/; revision=145

--- src/java/com/sun/jsftemplating/handlers/MetaDataHandlers.java
+++ src/java/com/sun/jsftemplating/handlers/MetaDataHandlers.java
@@ -31,6 +31,7 @@ import com.sun.jsftemplating.annotation.Handler;
 import com.sun.jsftemplating.annotation.HandlerInput;
 import com.sun.jsftemplating.annotation.HandlerOutput;
 import com.sun.jsftemplating.layout.LayoutDefinitionManager;
+import com.sun.jsftemplating.layout.descriptors.ComponentType;
 import com.sun.jsftemplating.layout.descriptors.handler.HandlerContext;
 import com.sun.jsftemplating.layout.descriptors.handler.HandlerDefinition;
 
@@ -64,11 +65,11 @@ public class MetaDataHandlers {
      *	    If the value is supplied, it will return information about that
      *	    handler only.  If not supplied, it will return information about
      *	    all handlers.  Output is passed via output values "info", "ids",
-     *	    and "handler".  Info is always returned and contains a String
-     *	    of information.  "ids" is always returned and contians a
-     *	    <code>Set</code> of global HandlerDefinition ids that may be
-     *	    passed into this method.  "handler" is returned only if an id was
-     *	    specified and will contain the requested
+     *	    and "handler".  "info" is always returned and contains a
+     *	    <code>String</code> of information.  "ids" is always returned and
+     *	    contains a <code>Set</code> of global {@link HandlerDefinition}
+     *	    ids that may be passed into this method.  "handler" is returned
+     *	    only if an id was specified and will contain the requested
      *	    {@link HandlerDefinition}.</p>
      */
     @Handler(id="getGlobalHandlerInformation",
@@ -78,7 +79,7 @@ public class MetaDataHandlers {
 	output={
 	    @HandlerOutput(name="info", type=String.class),
 	    @HandlerOutput(name="ids", type=List.class),
-	    @HandlerOutput(name="handler", type=HandlerDefinition.class)
+	    @HandlerOutput(name="value", type=HandlerDefinition.class)
 	})
     public static void getGlobalHandlerInformation(HandlerContext context) {
 	// Get the known global HandlerDefinitions
@@ -92,11 +93,60 @@ public class MetaDataHandlers {
 	// Produce a String of information also
 	String key = (String) context.getInputValue("id");
 	if (key != null) {
-	    context.setOutputValue("handler", defs.get(key));
+	    context.setOutputValue("value", defs.get(key));
 	    context.setOutputValue("info", defs.get(key).toString());
 	} else {
-	    Iterator it = defs.values().iterator();
-	    StringBuffer buf = new StringBuffer("=====\n");
+	    Iterator<HandlerDefinition> it = defs.values().iterator();
+	    StringBuffer buf = new StringBuffer("");
+	    while (it.hasNext()) {
+		buf.append(it.next().toString());
+	    }
+	    context.setOutputValue("info", buf.toString());
+	}
+    }
+
+    /**
+     *	<p> This handler provides information about all known (global)
+     *	    {@link ComponentType}s.  It allows an input value ("id") to be
+     *	    passed in, this is optional.  If the value is supplied, it will
+     *	    return information about that {@link ComponentType} only.  If not
+     *	    supplied, it will return information about all
+     *	    {@link ComponentType}s.  Output is passed via output values
+     *	    "info", "ids", and "value".  "info" is always returned and
+     *	    contains a <code>String</code> of information.  "ids" is always
+     *	    returned and contains a <code>Set</code> of global
+     *	    {@link ComponentType} ids that may be passed into this method.
+     *	    "value" is returned only if an id was specified and will contain
+     *	    the requested {@link ComponentType}.</p>
+     */
+    @Handler(id="getGlobalComponentTypeInformation",
+	input={
+	    @HandlerInput(name="id", type=String.class, required=false)
+	},
+	output={
+	    @HandlerOutput(name="info", type=String.class),
+	    @HandlerOutput(name="ids", type=List.class),
+	    @HandlerOutput(name="value", type=ComponentType.class)
+	})
+    public static void getGlobalComponentTypeInformation(HandlerContext context) {
+	// Get the known global HandlerDefinitions
+	Map<String, ComponentType> defs =
+	    LayoutDefinitionManager.getGlobalComponentTypes();
+
+	// Provide a Set of ids
+	context.setOutputValue("ids", defs.keySet());
+
+	// If a single HandlerDefinition was requested, provide it
+	// Produce a String of information also
+	String key = (String) context.getInputValue("id");
+	if (key != null) {
+	    // Return info for 1 ComponentType, return the type itself too
+	    context.setOutputValue("value", defs.get(key));
+	    context.setOutputValue("info", defs.get(key).toString());
+	} else {
+	    // Return info for ALL ComponentTypes
+	    Iterator<ComponentType> it = defs.values().iterator();
+	    StringBuffer buf = new StringBuffer("");
 	    while (it.hasNext()) {
 		buf.append(it.next().toString());
 	    }
