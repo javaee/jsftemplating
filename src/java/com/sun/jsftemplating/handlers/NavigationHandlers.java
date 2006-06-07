1c8a32d4d2fd2f3d6f3b7b06a2232ee95a3ccfe9
Ken Paulsen <kenpaulsen@java.net>
2006-06-07 09:16:40 +0000

    * Added handler to obtain a UIViewRoot.  This allows the developer to get   a UIViewRoot and then modify it (i.e. set a page session variable on it). * Modified the "navigate" handler to accept a UIViewRoot so the developer   may pass in a modified UIViewRoot (i.e. one w/ page session variables   set).
    svn path=/trunk/; revision=188

--- src/java/com/sun/jsftemplating/handlers/NavigationHandlers.java
+++ src/java/com/sun/jsftemplating/handlers/NavigationHandlers.java
@@ -29,6 +29,7 @@ package com.sun.jsftemplating.handlers;
 
 import com.sun.jsftemplating.annotation.Handler;
 import com.sun.jsftemplating.annotation.HandlerInput;
+import com.sun.jsftemplating.annotation.HandlerOutput;
 import com.sun.jsftemplating.layout.descriptors.handler.HandlerContext;
 
 import java.io.IOException;
@@ -53,22 +54,82 @@ public class NavigationHandlers {
     }
 
     /**
-     *	<p> This handler navigates to the given page.</p>
+     *	<p> This handler returns a <code>UIViewRoot</code>.  If the
+     *	    <code>id</code> parameter is supplied it will return the requested
+     *	    <code>UIViewRoot</code> (this may fail and cause an exception). If
+     *	    the <code>id</code> is <em>not</em> supplied, it will return the
+     *	    current <code>UIViewRoot</code>.  The result will be returned in
+     *	    an output parameter named <code>viewRoot</code>.</p>
+     */
+    @Handler(id="getUIViewRoot",
+	input={
+	    @HandlerInput(name="id", type=String.class)
+	},
+	output={
+	    @HandlerOutput(name="viewRoot", type=UIViewRoot.class)
+	})
+    public void getUIViewRoot(HandlerContext context) {
+	String pageName = (String) context.getInputValue("id");
+	FacesContext ctx = context.getFacesContext();
+	UIViewRoot root = null;
+	if (pageName == null) {
+	    root = ctx.getViewRoot();
+	} else {
+	    if (pageName.charAt(0) != '/') {
+		// Ensure we start w/ a '/'
+		pageName = "/" + pageName;
+	    }
+	    root = ctx.getApplication().getViewHandler().
+		createView(ctx, pageName);
+	}
+	context.setOutputValue("viewRoot", root);
+    }
+
+    /**
+     *	<p> This handler navigates to the given page.  <code>page</code> may
+     *	    either be a <code>UIViewRoot</code> or a <code>String</code>
+     *	    representing a <code>UIViewRoot</code>.  Passing in a
+     *	    <code>String</code> name of a <code>UIViewRoot</code> will always
+     *	    create a new <code>UIViewRoot</code>.  Passing in the
+     *	    <code>UIViewRoot</code> provides an opportunity to customize the
+     *	    <code>UIComponent</code> tree that will be displayed.</p>
      *
-     *	<p> Input value: "page" -- Type: <code>String</code></p>
+     *	<p> The {@link #getUIViewRoot(HandlerContext)} handler provides a way
+     *	    to obtain a <code>UIViewRoot</code>.</p>
+     *
+     *	<p> Input value: "page" -- Type: <code>Object</code> (should be a
+     *	    <code>String</code> or a <code>UIViewRoot</code>).</p>
      *
      *	@param	context	The {@link HandlerContext}.
      */
     @Handler(id="navigate",
 	input={
-	    @HandlerInput(name="page", type=String.class, required=true)
+	    @HandlerInput(name="page", type=Object.class, required=true)
 	})
     public static void navigate(HandlerContext context) {
-	String page = (String) context.getInputValue("page");
+	Object page = context.getInputValue("page");
+	UIViewRoot root = null;
 	FacesContext ctx = context.getFacesContext();
-	UIViewRoot newRoot = ctx.getApplication().getViewHandler().
-	    createView(ctx, page);
-	ctx.setViewRoot(newRoot);
+	if (page instanceof String) {
+	    // Create a new UIViewRoot with the given id
+	    String strPage = (String) page;
+	    if (strPage.charAt(0) != '/') {
+		// Ensure we start w/ a '/'
+		strPage = "/" + strPage;
+	    }
+	    root = ctx.getApplication().getViewHandler().
+		createView(ctx, strPage);
+	} else if (page instanceof UIViewRoot) {
+	    // We recieved a UIViewRoot, use it...
+	    root = (UIViewRoot) page;
+	} else {
+	    throw new IllegalArgumentException("Type '"
+		+ page.getClass().getName()
+		+ "' is not valid.  It must be a String or UIViewRoot.");
+	}
+
+	// Set the UIViewRoot so that it will be displayed
+	ctx.setViewRoot(root);
     }
 
     /**
