f9a553d2cfe90632e8bd7c073aaff1016bb62282
Ken Paulsen <kenpaulsen@java.net>
2007-01-22 11:43:59 +0000

    * Added handlers for the following purposes:   - replaceUIComponent: Allows you to swap one UIComponent for another.     Since UIComponent order is important, this is a key feature to     replacing dynamically changing content when the structure of the     UIComponent tree may change.  It also allows you to create a new     UIComponent vs. updating an existing one.  This is desirable if you     want to re-execute ComponentFactory code.   - buildUIComponentTree: This is actually moved from MetaDataHandlers, but     it is also enhanced.  This not returns the "result" and also allows the     current LayoutComponent to be recreated.  This is critical if you want     to update a single UIComponent that has peers (previously you had to     update all the parent's children).   - createComponent: This allows you to execute a ComponentFactory via a     handler.  This is useful for many things, one of which is that it     allows you to create a new component and pass it to handlers such as     the "replaceUIComponent" handler above.
    svn path=/trunk/; revision=483

--- src/java/com/sun/jsftemplating/handlers/ComponentHandlers.java
+++ src/java/com/sun/jsftemplating/handlers/ComponentHandlers.java
@@ -30,11 +30,19 @@ package com.sun.jsftemplating.handlers;
 import com.sun.jsftemplating.annotation.Handler;
 import com.sun.jsftemplating.annotation.HandlerInput;
 import com.sun.jsftemplating.annotation.HandlerOutput;
+import com.sun.jsftemplating.component.ComponentUtil;
+import com.sun.jsftemplating.layout.LayoutDefinitionManager;
+import com.sun.jsftemplating.layout.LayoutViewHandler;
+import com.sun.jsftemplating.layout.LayoutViewRoot;
 import com.sun.jsftemplating.layout.descriptors.handler.HandlerContext;
+import com.sun.jsftemplating.layout.descriptors.LayoutComponent;
+import com.sun.jsftemplating.layout.descriptors.LayoutElement;
+import com.sun.jsftemplating.util.LayoutElementUtil;
 
 import java.util.List;
 
 import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
 
 
 /**
@@ -74,6 +82,188 @@ public class ComponentHandlers {
 	List<UIComponent> list = parent.getChildren();
 	context.setOutputValue("children", list);
 	context.setOutputValue("size", new Integer(list.size()));
+    }
+
+    /**
+     *	<p> This handler replaces the given <code>old UIComponent</code> in the
+     *	    <code>UIComponent</code> tree with the given <code>new
+     *	    UIComponent</code>.  If the new <code>UIComponent</code> is not
+     *	    specified or is <code>null</code>, the old UIComponent will simply
+     *	    be removed.</p>
+     *
+     *	<p> Input value: "old" -- Type: <code>UIComponent</code></p>
+     *	<p> Input value: "new" -- Type: <code>UIComponent</code></p>
+     *
+     *	@param	context	The <code>HandlerContext</code>.
+     */
+    @Handler(id="replaceUIComponent",
+	input={
+	    @HandlerInput(name="old", type=UIComponent.class, required=true),
+	    @HandlerInput(name="new", type=UIComponent.class, required=false)}
+	)
+    public static void replaceUIComponent(HandlerContext context) {
+	// Get the old component which is to be replaced
+	UIComponent oldComp = (UIComponent) context.getInputValue("old");
+	if (oldComp == null) {
+	    throw new IllegalArgumentException(
+		    "You must provide a non-null value for 'component'.");
+	}
+
+	// Check for a replacement UIComponent
+	UIComponent newComp = (UIComponent) context.getInputValue("new");
+
+	// Get the child UIComponent list...
+	List<UIComponent> list = oldComp.getParent().getChildren();
+	if (newComp == null) {
+	    // Nothing to replace it with, just do a remove...
+	    list.remove(oldComp);
+	} else {
+	    // Find the index to put the new UIComponent in the right place
+	    int index = list.indexOf(oldComp);
+	    list.set(index, newComp);
+	}
+    }
+
+    /**
+     *	<p> This will build a <code>UIComponent</code> tree from a
+     *	    {@link LayoutElement}.  You must pass in the {@link LayoutElement}
+     *	    that will be used to create the <code>UIComponent</code> tree.
+     *	    You may optionally pass in the <code>parent UIComponent</code>
+     *	    which will serve as the parent for the newly created
+     *	    <code>UIComponent</code> tree.  The resulting
+     *	    <code>UIComponent</code> tree will be returned via the
+     *	    <code>result</code> output value.  If more than 1 root node exists
+     *	    for the given <code>LayoutElement</code>, the last added to the
+     *	    <code>parent</code> will be returned.  Typically, you will pass in
+     *	    a {@link LayoutComponent} as the <code>layoutElement</code> so
+     *	    there will only be 1.</p>
+     *
+     *	<p> It is recommended that you *do* supply the parent since EL
+     *	    expressions may depend on this when creating the
+     *	    <code>UIComponent</code> tree.</p>
+     *
+     *	<p> One possible use case for calling this method would be to have a
+     *	    dynamic "id" property of a {@link LayoutComponent}, call this
+     *	    method multiple times with different values set in the "id"
+     *	    property.  Remember, that you should not change a
+     *	    {@link LayoutComponent} (or any {@link LayoutElement}) directly.
+     *	    It is only safe to have dynamic values through EL bindings #{}.</p>
+     *
+     *	<p> Another reason to use this handler is to cause a portion of a
+     *	    <code>UIComponent</code> tree to be recreated.  This of often
+     *	    desirable during Ajax requests so that factory options can be
+     *	    reevaluated.</p>
+     *
+     *	<p> Input value: "layoutElement" -- Type: <code>LayoutElement</code></p>
+     *	<p> Input value: "parent"	 -- Type: <code>UIComponent</code></p>
+     *
+     *	<p> Output value: "result"     -- Type: <code>UIComponent</code></p>
+     *
+     *	@param	context	The HandlerContext.
+     */
+    @Handler(id="buildUIComponentTree",
+	input={
+	    @HandlerInput(name="layoutElement", type=LayoutElement.class, required=true),
+	    @HandlerInput(name="parent", type=UIComponent.class, required=false)},
+	output={
+	    @HandlerOutput(name="result", type=UIComponent.class)}
+	)
+    public static void buildUIComponentTree(HandlerContext context) {
+	LayoutElement desc = (LayoutElement) context.getInputValue("layoutElement");
+	UIComponent parent = (UIComponent) context.getInputValue("parent");
+
+	// If they didn't give us a parent, make one one up...
+	if (parent == null) {
+	    parent = new LayoutViewRoot();
+	    ((LayoutViewRoot) parent).setViewId("fake");
+	}
+
+	// Build it...
+	FacesContext facesCtx = context.getFacesContext();
+	if (desc instanceof LayoutComponent) {
+	    // Special case if LayoutComponent is the root node element
+	    // The LayoutViewHandler assumes that the root node is not used
+	    // because it was written assuming that the LayoutDefinition was
+	    // always going to be passed in.  The result is that it ignores
+	    // the LayoutElement that is passed in and goes striaight to the
+	    // children.  This isn't what we want.  Process the child here,
+	    // then continue as normal.
+	    UIComponent tmpParent =
+		((LayoutComponent) desc).getChild(facesCtx, parent);
+	    LayoutViewHandler.buildUIComponentTree(facesCtx, tmpParent, desc);
+	} else {
+	    // Process normally (which in this path is probably not normal)
+	    LayoutViewHandler.buildUIComponentTree(facesCtx, parent, desc);
+	}
+
+	// Get the result to return...
+	String id = desc.getId(facesCtx, parent);
+	UIComponent result = parent.findComponent(id);
+	if (result == null) {
+	    // First see if we can find it in the facet Map
+	    if (desc instanceof LayoutComponent) {
+		result = parent.getFacets().get(id);
+	    } else {
+// FIXME: find a way to find a "facet child" if the root LayoutElement is not a LayoutComponent
+	    }
+
+	    if (result == null) {
+		// Still not found, check children...
+		List<UIComponent> children = parent.getChildren();
+		if (children.size() > 0) {
+		    // Return the last child added b/c we want to make sure
+		    // we're returning something that we added, not something
+		    // that already existed.  While not perfect, this is
+		    // reasonable.
+		    result = children.get(children.size() -1);
+		}
+	    }
+	}
+
+	// Set the output...
+	context.setOutputValue("result", result);
+    }
+
+    /**
+     *	<p> This handler creates a <code>UIComponent</code>.  It requires you
+     *	    to pass in the componentType (<code>type</code>) and returns the
+     *	    new component via the output parameter <code>component</code>.</p>
+     *
+     *	<p> Input value: "type"	      -- Type: <code>String</code></p>
+     *	<p> Input value: "parent"     -- Type: <code>UIComponent</code></p>
+     *
+     *	<p> Output value: "component" -- Type: <code>UIComponent</code></p>
+     *
+     *	@param	context	The {@link HandlerContext}.
+     */
+    @Handler(id="createComponent",
+	input={
+	    @HandlerInput(name="type", type=String.class, required=true),
+	    @HandlerInput(name="id", type=String.class, required=false),
+	    @HandlerInput(name="parent", type=UIComponent.class, required=false)},
+	output={
+	    @HandlerOutput(name="component", type=UIComponent.class)}
+	)
+    public static void createComponent(HandlerContext context) {
+	// Get the input...
+	String type = (String) context.getInputValue("type");
+	UIComponent parent = (UIComponent) context.getInputValue("parent");
+	String id = (String) context.getInputValue("id");
+	if (id == null) {
+	    id = LayoutElementUtil.getGeneratedId(type);
+	}
+
+	// Create a LayoutComponent...
+	LayoutComponent desc = new LayoutComponent((LayoutComponent) null, id,
+		LayoutDefinitionManager.getGlobalComponentType(type));
+	desc.setFacetChild(false);
+
+	// Create the component...
+	UIComponent component = ComponentUtil.createChildComponent(
+		context.getFacesContext(), desc, parent);
+
+	// Return the result...
+	context.setOutputValue("component", component);
     }
 
     /**
