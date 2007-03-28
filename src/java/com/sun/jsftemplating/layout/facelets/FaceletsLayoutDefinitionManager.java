c10e6c7e597cd2946c393cf6643485f379b97274
Jason Lee <jdlee@java.net>
2007-03-28 19:35:50 +0000

    Added the LDM and reader for the new/proposed Facelets-compatible format
    svn path=/trunk/; revision=610

---
+++ src/java/com/sun/jsftemplating/layout/facelets/FaceletsLayoutDefinitionManager.java
@@ -0,0 +1,121 @@
+/**
+ * 
+ */
+package com.sun.jsftemplating.layout.facelets;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.faces.context.FacesContext;
+
+import com.sun.jsftemplating.annotation.FormatDefinition;
+import com.sun.jsftemplating.layout.LayoutDefinitionException;
+import com.sun.jsftemplating.layout.LayoutDefinitionManager;
+import com.sun.jsftemplating.layout.descriptors.LayoutDefinition;
+import com.sun.jsftemplating.layout.template.TemplateParser;
+import com.sun.jsftemplating.util.FileUtil;
+
+/**
+ * @author Jason Lee
+ *
+ */
+@FormatDefinition
+public class FaceletsLayoutDefinitionManager extends LayoutDefinitionManager {
+
+    public FaceletsLayoutDefinitionManager() {
+        super();
+    }
+
+    @Override
+    public boolean accepts(String key) {
+        URL url = FileUtil.searchForFile(key, ".jsf");
+        if (url == null) {
+            return false;
+        }
+
+        // Use the TemplateParser to help us read the file to see if it is a
+        // valid XML-format file
+        TemplateParser parser = new TemplateParser(url);
+        try {
+            parser.open();
+            parser.readUntil("<ui:composition", true);
+        } catch (Exception ex) {
+            // Didn't work...
+            return false;
+        } finally {
+            parser.close();
+        }
+        return true;
+    }
+
+    /**
+     *  <p> This method is responsible for finding the requested
+     *      {@link LayoutDefinition} for the given <code>key</code>.</p>
+     *
+     *  @param  key     Key identifying the desired {@link LayoutDefinition}.
+     *
+     *  @return         The requested {@link LayoutDefinition}.
+     */
+    public LayoutDefinition getLayoutDefinition(String key) throws LayoutDefinitionException {
+        // Remove leading "/" (we'll add it back if needed)
+        while (key.startsWith("/")) {
+            key = key.substring(1);
+        }
+
+        // See if we already have this one.
+        LayoutDefinition ld = getCachedLayoutDefinition(key);
+        if (ld == null) {
+            URL url = FileUtil.searchForFile(key, ".jsf");
+
+            // Make sure we found the url
+            if (url == null) {
+                throw new LayoutDefinitionException(
+                        "Unable to locate '" + key + "'");
+            }
+
+            // Read the template file
+            try {
+                ld  = new FaceletsLayoutDefinitionReader(key, url).read();
+            } catch (IOException ex) {
+                throw new LayoutDefinitionException(
+                    "Unable to process '" + url.toString() + "'.", ex);
+            }
+
+            // Cache the LayoutDefinition
+            putCachedLayoutDefinition(key, ld);
+        }
+
+        // Dispatch "initPage" handlers
+        ld.dispatchInitPageHandlers(FacesContext.getCurrentInstance(), ld);
+
+        // Return the LayoutDefinition
+        return ld;
+    }
+
+    /**
+     *  <p> This method returns an instance of this LayoutDefinitionManager.
+     *      The object returned is a singleton (only 1 instance will be
+     *      created per JVM).</p>
+     *
+     *  @return <code>XMLLayoutDefinitionManager</code> instance
+     */
+    public static LayoutDefinitionManager getInstance() {
+        return SingletonHolder.instance;
+    }
+
+    /**
+     *  <p> This is used to ensure that only 1 instance of this class is
+     *      created (per JVM).</p>
+     */
+    static class SingletonHolder {
+        static FaceletsLayoutDefinitionManager instance = new FaceletsLayoutDefinitionManager();
+    }
+
+    public static void main(String... args) {
+        FaceletsLayoutDefinitionManager ldm = new  FaceletsLayoutDefinitionManager();
+        if (ldm.accepts("/com/sun/jsftemplating/layout/facelets/index.xhtml")) {
+            LayoutDefinition ld = ldm.getLayoutDefinition("/com/sun/jsftemplating/layout/facelets/index.xhtml");
+            System.out.println(ld);
+        }
+    }
+}
---
+++ src/java/com/sun/jsftemplating/layout/facelets/FaceletsLayoutDefinitionReader.java
@@ -0,0 +1,129 @@
+/**
+ * 
+ */
+package com.sun.jsftemplating.layout.facelets;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import com.sun.jsftemplating.layout.LayoutDefinitionException;
+import com.sun.jsftemplating.layout.LayoutDefinitionManager;
+import com.sun.jsftemplating.layout.descriptors.ComponentType;
+import com.sun.jsftemplating.layout.descriptors.LayoutComponent;
+import com.sun.jsftemplating.layout.descriptors.LayoutDefinition;
+import com.sun.jsftemplating.layout.descriptors.LayoutElement;
+import com.sun.jsftemplating.layout.descriptors.LayoutStaticText;
+
+/**
+ * @author Jason Lee
+ *
+ */
+public class FaceletsLayoutDefinitionReader {
+    private URL url;
+    private String key;
+    private Document document;
+
+    public FaceletsLayoutDefinitionReader(String key, URL url) {
+        try{
+            this.url = url;
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            dbf.setNamespaceAware(true);
+            dbf.setValidating(false);
+            dbf.setIgnoringComments(true);
+            dbf.setIgnoringElementContentWhitespace(false);
+            dbf.setCoalescing(false);
+            // The opposite of creating entity ref nodes is expanding inline
+            dbf.setExpandEntityReferences(true);
+
+            DocumentBuilder builder = dbf.newDocumentBuilder();
+            builder.setErrorHandler(new ParsingErrorHandler());
+            InputStream is = new BufferedInputStream(this.url.openStream());
+            document = builder.parse(is);
+            is.close();
+        } catch (Exception e) {
+            throw new LayoutDefinitionException(e);
+        }
+    }
+
+    public LayoutDefinition read() throws IOException {
+        LayoutDefinition ld = new LayoutDefinition(key);
+        NodeList nodeList = document.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            process(ld, nodeList.item(i));
+        }
+        return ld;
+    }
+
+    public LayoutElement process(LayoutElement parent, Node node) throws IOException {
+        LayoutElement element = null;
+
+        String value = node.getNodeValue();
+        if (value != null) {
+            value = value.trim();
+        } else {
+//            value = "";
+        }
+        System.out.println(node.getNodeName() + ":  '" + node.getNodeType() + "' = '" + value + "'");
+        // TODO:  find out what "name" should be in the ctors
+        switch (node.getNodeType()) {
+        case Node.TEXT_NODE :
+            element = new LayoutStaticText(parent, "", node.getNodeValue());
+            break;
+        case Node.ELEMENT_NODE:
+            ComponentType componentType = LayoutDefinitionManager.  getGlobalComponentType(node.getNodeName());
+            element = new LayoutComponent(parent, "", componentType);
+            break;
+        default:
+            // just because... :P
+        }
+
+
+        NodeList nodeList = node.getChildNodes();
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            process(element, nodeList.item(i));
+        }
+
+        if (parent != null) {
+            parent.addChildLayoutElement(element);
+        }
+        return element;
+    }
+}
+
+
+class ParsingErrorHandler implements org.xml.sax.ErrorHandler {
+    //Log logger = LogFactory.getLog(this.getClass());
+
+    public ParsingErrorHandler() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    public void warning(SAXParseException arg0) throws SAXException {
+//      logger.warn(arg0.getMessage());
+    }
+
+    public void error(SAXParseException arg0) throws SAXException {
+        //logger.error(arg0.getMessage());
+        fatalError(arg0);
+    }
+
+    public void fatalError(SAXParseException arg0) throws SAXException {
+//      logger.error(arg0.getMessage());
+        System.err.println (arg0.getMessage());
+        System.exit(-1);
+    }
+
+}
