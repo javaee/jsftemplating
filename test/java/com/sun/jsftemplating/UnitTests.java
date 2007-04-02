eb1bdbd5aec86547851677694ba6348338f06e1f
Ken Paulsen <kenpaulsen@java.net>
2007-04-02 23:29:43 +0000

    * Added copyright, removed extra import
    svn path=/trunk/; revision=635

--- test/java/com/sun/jsftemplating/UnitTests.java
+++ test/java/com/sun/jsftemplating/UnitTests.java
@@ -1,11 +1,31 @@
+/*
+ * The contents of this file are subject to the terms 
+ * of the Common Development and Distribution License 
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at 
+ * https://jsftemplating.dev.java.net/cddl1.html or
+ * jsftemplating/cddl1.txt.
+ * See the License for the specific language governing 
+ * permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL 
+ * Header Notice in each file and include the License file 
+ * at jsftemplating/cddl1.txt.  
+ * If applicable, add the following below the CDDL Header, 
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
+ */
 package com.sun.jsftemplating;
 
 import com.sun.jsftemplating.layout.LayoutDefinitionManagerTest;
 import com.sun.jsftemplating.layout.template.TemplateParserTest;
 import com.sun.jsftemplating.layout.template.TemplateReaderTest;
 import com.sun.jsftemplating.layout.template.TemplateWriterTest;
-
-import java.net.URL;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
