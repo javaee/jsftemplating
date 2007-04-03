fdf813f9d565495c1d56ef0f68462bcab4721b40
Ken Paulsen <kenpaulsen@java.net>
2007-04-03 19:17:34 +0000

    * Forgot to checkin this file.
    svn path=/trunk/; revision=652

--- test/java/com/sun/jsftemplating/UnitTests.java
+++ test/java/com/sun/jsftemplating/UnitTests.java
@@ -23,6 +23,7 @@
 package com.sun.jsftemplating;
 
 import com.sun.jsftemplating.layout.LayoutDefinitionManagerTest;
+import com.sun.jsftemplating.layout.facelets.FaceletsLayoutDefinitionReaderTest;
 import com.sun.jsftemplating.layout.template.TemplateParserTest;
 import com.sun.jsftemplating.layout.template.TemplateReaderTest;
 import com.sun.jsftemplating.layout.template.TemplateWriterTest;
@@ -45,6 +46,7 @@ public class UnitTests {
 	tests.addTestSuite(TemplateParserTest.class);
 	tests.addTestSuite(TemplateReaderTest.class);
 	tests.addTestSuite(TemplateWriterTest.class);
+	tests.addTestSuite(FaceletsLayoutDefinitionReaderTest.class);
 	tests.addTestSuite(LayoutDefinitionManagerTest.class);
 	return tests;
     }
