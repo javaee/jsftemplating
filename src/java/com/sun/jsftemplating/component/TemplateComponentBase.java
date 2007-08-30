345b0e1285561398923a4aad23e76fa18f05da63
Ken Paulsen <kenpaulsen@java.net>
2007-08-30 22:18:51 +0000

    * Updated JavaDoc.
    svn path=/trunk/; revision=833

--- src/java/com/sun/jsftemplating/component/TemplateComponentBase.java
+++ src/java/com/sun/jsftemplating/component/TemplateComponentBase.java
@@ -18,7 +18,7 @@
  * you own identifying information: 
  * "Portions Copyrighted [year] [name of copyright owner]"
  * 
- * Copyright 2006 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2007 Sun Microsystems, Inc. All rights reserved.
  */
 package com.sun.jsftemplating.component;
 
@@ -36,7 +36,11 @@ import com.sun.jsftemplating.layout.descriptors.LayoutElement;
 /**
  *  <p>	This abstract class provides base functionality for components that
  *	work in conjunction with the
- *	{@link com.sun.jsftemplating.renderer.TemplateRenderer}.  It
+ *	{@link com.sun.jsftemplating.renderer.TemplateRenderer} that do not
+ *	need any "special" component features (such as provided by
+ *	<code>UIInput</code>, or an <code>ActionSource</code> implementation).
+ *	This class is a suitable class for "container" types of components, or
+ *	components that simply agregate other components together.  It
  *	provides a default implementation of the
  *	{@link com.sun.jsftemplating.component.TemplateComponent}
  *	interface.</p>
@@ -49,14 +53,14 @@ import com.sun.jsftemplating.layout.descriptors.LayoutElement;
 public abstract class TemplateComponentBase extends UIComponentBase implements TemplateComponent {
 
     /**
-     *	This method will find the request child UIComponent by id.  If it is
-     *	not found, it will attempt to create it if it can find a LayoutElement
-     *	describing it.
+     *	<p> This method will find the request child <code>UIComponent</code>
+     *	    by id.  If it is not found, it will attempt to create it if it can
+     *	    find a {@link LayoutElement} describing it.</p>
      *
-     *	@param	context	    The FacesContext
-     *	@param	id	    The UIComponent id to search for
+     *	@param	context	    The <code>FacesContext</code>.
+     *	@param	id	    The <code>UIComponent</code> id to find.
      *
-     *	@return	The requested UIComponent
+     *	@return	The requested <code>UIComponent</code>.
      */
     public UIComponent getChild(FacesContext context, String id) {
 	return getHelper().getChild(this, context, id);
@@ -64,58 +68,60 @@ public abstract class TemplateComponentBase extends UIComponentBase implements T
 
 
     /**
-     *	This method will find the request child UIComponent by id (the id is
-     *	obtained from the given LayoutComponent).  If it is not found, it will
-     *	attempt to create it from the supplied LayoutElement.
+     *	<p> This method will find the request child <code>UIComponent</code>
+     *	    by id.  If it is not found, it will attempt to create it if it can
+     *	    find a {@link LayoutElement} describing it.</p>
      *
-     *	@param	descriptor  The LayoutElement describing the UIComponent
+     *	@param	context	    The <code>FacesContext</code>.
+     *	@param	id	    The <code>UIComponent</code> id to find.
      *
-     *	@return	The requested UIComponent
+     *	@return	The requested <code>UIComponent</code>.
      */
     public UIComponent getChild(FacesContext context, LayoutComponent descriptor) {
 	return getHelper().getChild(this, context, descriptor);
     }
 
     /**
-     *	This method returns the LayoutDefinition associated with this component.
+     *	<p> This method returns the {@link LayoutDefinition} associated with
+     *	    this component.</p>
      *
-     *	@param	context	The FacesContext
+     *	@param	context	The <code>FacesContext</code>.
      *
-     *	@return	LayoutDefinition associated with this component.
+     *	@return	{@link LayoutDefinition} associated with this component.
      */
     public LayoutDefinition getLayoutDefinition(FacesContext context) {
 	return getHelper().getLayoutDefinition(context);
     }
 
     /**
-     *	This method saves the state for this component.  It relies on the
-     *	super class to save its own sate, this method will invoke
-     *	super.saveState().
+     *	<p> This method saves the state for this component.  It relies on the
+     *	    superclass to save its own sate, this method will invoke
+     *	    super.saveState().</p>
      *
-     *	@param	context	The FacesContext
+     *	@param	context	The <code>FacesContext</code>.
      *
-     *	@return The serialized State
+     *	@return The serialized state.
      */
     public Object saveState(FacesContext context) {
 	return getHelper().saveState(context, super.saveState(context));
     }
 
     /**
-     *	This method restores the state for this component.  It will invoke the
-     *	super class to restore its state.
+     *	<p> This method restores the state for this component.  It will invoke
+     *	    the superclass to restore its state.</p>
      *
-     *	@param	context	The FacesContext
-     *	@param	state	The serialized State
-     *
+     *	@param	context	The <code>FacesContext</code>.
+     *	@param	state	The serialized state.
      */
     public void restoreState(FacesContext context, Object state) {
 	super.restoreState(context, getHelper().restoreState(context, state));
     }
 
     /**
-     *	This method returns the LayoutDefinitionKey for this component.
+     *	<p> This method returns the {@link LayoutDefinition} key for this
+     *	    component.</p>
      *
-     *	@return	key	The key to use in the {@link LayoutDefinitionManager}.
+     *	@return	The key to use in the {@link LayoutDefinitionManager}.
      */
     public String getLayoutDefinitionKey() {
 	return getHelper().getLayoutDefinitionKey();
@@ -123,7 +129,8 @@ public abstract class TemplateComponentBase extends UIComponentBase implements T
 
 
     /**
-     *	This method sets the LayoutDefinition key for this component.
+     *	<p> This method sets the {@link LayoutDefinition} key for this
+     *	    component.</p>
      *
      *	@param	key The key to use in the {@link LayoutDefinitionManager}.
      */
@@ -135,6 +142,8 @@ public abstract class TemplateComponentBase extends UIComponentBase implements T
      *	<p> This method retrieves the {@link TemplateComponentHelper} used by
      *	    this class to help implement the {@link TemplateComponent}
      *	    interface.</p>
+     *
+     *	@return	The {@link TemplateComponentHelper} for this component.
      */
     protected TemplateComponentHelper getHelper() {
 	if (_helper == null) {
@@ -145,7 +154,7 @@ public abstract class TemplateComponentBase extends UIComponentBase implements T
 
     /**
      *	<p> Our <code>TemplateComponentHelper</code>.  We initialize it on
-     *	    access b/c we want to ensure it exists, if it is Serialized it
+     *	    access b/c we want to ensure it exists, if it is serialized it
      *	    won't exist if we init it here or in the constructor.</p>
      */
     private transient TemplateComponentHelper _helper = null;
