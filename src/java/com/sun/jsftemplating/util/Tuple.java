4eeab34212a201ff6864649eae22188793dc26a0
Ken Paulsen <kenpaulsen@java.net>
2009-04-09 23:09:21 +0000

    * Added license header.
    svn path=/trunk/; revision=1374

--- src/java/com/sun/jsftemplating/util/Tuple.java
+++ src/java/com/sun/jsftemplating/util/Tuple.java
@@ -1,13 +1,31 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * The contents of this file are subject to the terms 
+ * of the Common Development and Distribution License 
+ * (the License).  You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the license at 
+ * https://jsftemplating.dev.java.net/cddl1.html or
+ * jsftemplating/cddl1.txt.
+ * See the License for the specific language governing 
+ * permissions and limitations under the License.
+ * 
+ * When distributing Covered Code, include this CDDL 
+ * Header Notice in each file and include the License file 
+ * at jsftemplating/cddl1.txt.  
+ * If applicable, add the following below the CDDL Header, 
+ * with the fields enclosed by brackets [] replaced by
+ * you own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
  */
-
 package com.sun.jsftemplating.util;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
 
 /**
  *
@@ -28,5 +46,4 @@ public class Tuple {
     public List<Object> getElements() {
         return Collections.unmodifiableList(elements);
     }
-    
 }
